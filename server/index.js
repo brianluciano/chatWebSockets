92bc4c0f1796a84d85468d62c0add8272f63c9d1
Brian <brian.comas@4rsoluciones.com>
2024-02-14 11:23:36 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {
+res.send('<h1>Esto es el chat</h1>');
+});
+
+app.listen(port, ()=> {
+    console.log(`Server runint on port ${port}`)
+})
